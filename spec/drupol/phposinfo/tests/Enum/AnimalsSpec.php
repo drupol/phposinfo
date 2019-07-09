a58ed4ef0b6b89bebb79e8d2940e95b87bc486b8
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-07-09 08:24:50 +0200

    Increase tests coverage.

---
+++ spec/drupol/phposinfo/tests/Enum/AnimalsSpec.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace spec\drupol\phposinfo\tests\Enum;
+
+use drupol\phposinfo\tests\Enum\Animals;
+use PhpSpec\ObjectBehavior;
+
+class AnimalsSpec extends ObjectBehavior
+{
+    public function it_can_get_a_key()
+    {
+        $this::has('DOGS')
+            ->shouldReturn(true);
+
+        $this::has('FOO')
+            ->shouldReturn(false);
+
+        $this::isValid('COIN')
+            ->shouldReturn(false);
+
+        $this::isValid('cats')
+            ->shouldReturn(true);
+
+        $this::key('dogs')
+            ->shouldReturn('DOGS');
+
+        $this::value('DOGS')
+            ->shouldReturn('dogs');
+
+        $this
+            ->shouldThrow(\Exception::class)
+            ->during('value', ['FOO']);
+
+        $this
+            ->shouldThrow(\Exception::class)
+            ->during('key', ['foo']);
+
+        $this::list()
+            ->shouldYieldLike(['CATS' => 'cats', 'DOGS' => 'dogs']);
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(Animals::class);
+    }
+}
